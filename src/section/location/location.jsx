e64b34048147270e053524f7345359eaa3af2508
Your Name <hoangtan250997@gmail.com>
2023-11-29 01:07:23 +0700

    image list

--- src/section/location/location.jsx
+++ src/section/location/location.jsx
@@ -2,21 +2,47 @@ import React from "react";
 import "./location.scss";
 import Image from "react-bootstrap/Image";
 import Van from "./van.jpg";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Location = () => {
   return (
-    <div className="sample">
-      <Image src={Van} roundedCircle id="Van" />
-
-      {/* <div className="header-pic"></div>
-      <div className="section1-content">
-        <span className="sub-content">Save Our Date</span>
-        <h1 className="main-content">Lê Sang & Thanh Vân</h1>
-        <span className="sub-content">24.12.2023</span>
-      </div>
-      {/* <a data-scroll className="fixed-button" href="#rsvp" id="rsvpButton">
-        RSVP
-      </a> */}
+    <div className="location">
+      <Row>
+        <Col xs={6} md={6}>
+          <Row>
+            <Image id="Sang" src={Van} roundedCircle />
+          </Row>
+          <Row>
+            <div className="content">
+              <h1 className="main-content">Nhà trai</h1>
+              <p className="sub-content">11:30 Chủ nhật, ngày 24/12/2023</p>
+              <p className="sub-content">(Tức ngày 12 tháng 11 năm Quý Mão)</p>
+              <br />
+              <p className="sub-content">
+                Tại:Khuôn viên Nhà máy cao su Long Thành Khu 14 xã Long Đức, LT,
+                ĐN
+              </p>
+            </div>{" "}
+          </Row>
+        </Col>
+        <Col xs={6} md={6}>
+          <Row>
+            <Image id="Van" src={Van} roundedCircle />
+          </Row>
+          <Row>
+            <div className="content">
+              <h1 className="main-content">Nhà Gái</h1>
+              <p className="sub-content">11:30 Thứ bảy, ngày 23/12/2023</p>
+              <p className="sub-content">(Tức ngày 11 tháng 11 năm Quý Mão)</p>
+              <br />
+              <p className="sub-content">
+                Tại: Tư Gia Ấp xóm Đình, xã Bình Sơn, LT, ĐN
+              </p>
+            </div>{" "}
+          </Row>
+        </Col>
+      </Row>
     </div>
   );
 };
--- src/section/time/time.jsx
+++ src/section/time/time.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./time.scss";
 
 const Time = () => {
+  const [timeLeft, setTimeLeft] = useState({});
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const difference = new Date("2023-12-24") - new Date();
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((difference / 1000 / 60) % 60);
+      const seconds = Math.floor((difference / 1000) % 60);
+      setTimeLeft({ days, hours, minutes, seconds });
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
   return (
     <div className="time">
       <div className="content">
-        <span className="sub-content">Save Our Date</span>
-        <h1 className="main-content">Lê Sang & Thanh Vân</h1>
-        <span className="sub-content">24.12.2023</span>
+        <span className="main-content">Đám cưới của chúng mình</span>
+        <h1 className="sub-content">Ngày 24 tháng 12 năm 2023</h1>
+        <span className="main-content">
+          {" "}
+          {timeLeft.days} ngày {timeLeft.hours} giờ {timeLeft.minutes} phút{" "}
+          {timeLeft.seconds} giây
+        </span>
       </div>
     </div>
   );
